e9e55c8ac4a4b1ee1e0df82ccd683f9164e89684
Rachelle Keblitis <rkeblitis@gmail.com>
2015-04-29 14:01:29 -0700

    add helper method to sort jobs by created_at time

--- app/helpers/job_board_helper.rb
+++ app/helpers/job_board_helper.rb
@@ -1,2 +1,7 @@
 module JobBoardHelper
+
+  def sort_jobs(jobs_array)
+    jobs_array = Job.all.order("created_at DESC")
+  end
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ActiveRecord::Base
+
+  # def sort_jobs
+  #   jobs = Job.all.order("created_at DESC")
+  # end
+
 end
