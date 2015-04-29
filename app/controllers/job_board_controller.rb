c84a819eae3acbd5bb40b0022d9cc6d64589697b
Rachelle Keblitis <rkeblitis@gmail.com>
2015-04-28 22:55:38 -0700

    remove turbolinks and successfully add jobs to database

--- app/controllers/job_board_controller.rb
+++ app/controllers/job_board_controller.rb
@@ -6,7 +6,6 @@ class JobBoardController < ApplicationController
   end
 
   def create
-    raise
     @job = Job.create(params.require(:job).permit(:title, :location, :start_date, :salary, :description))
     redirect_to jobs_path
   end
