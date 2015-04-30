d550d2cc209359140ef11729c7f0924671b4eae1
Rachelle Keblitis <rkeblitis@gmail.com>
2015-04-29 17:48:49 -0700

    make helper to format start_date to m/d/y

--- app/helpers/job_board_helper.rb
+++ app/helpers/job_board_helper.rb
@@ -4,9 +4,9 @@ module JobBoardHelper
     jobs_array = Job.all.order("created_at DESC")
   end
 
-  def days_posted(date)
+  def time_elasped(created_at)
     # get elasped time in days, and make it a postive number:
-    time_elasped = (((date - Time.now)/ 1.day).round) * -1
+    time_elasped = (((created_at - Time.now)/ 1.day).round) * -1
     if time_elasped == 0
       return "Today"
     elsif time_elasped == 1
@@ -14,5 +14,10 @@ module JobBoardHelper
     else
       return "#{time_elasped} days ago"
     end
+  end
+
+  def convert_to_pst(start_date)
+    date = Date.parse(start_date.to_s)
+    formated_date = "#{date.month}/#{date.day}/#{date.year}"
   end
 end
